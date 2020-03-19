56785138387d82d4f4f58693598d78ea68e92e56
schilkyl <schilkyl@gmail.com>
2020-03-18 23:21:23 -0500

    only move folder if it is being created "new" and not as a child of another folder

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 const TOOLBAR_FOLDER_ID = 'toolbar_____';
+const UNFILED_ID = 'unfiled_____';
 
-browser.bookmarks.onCreated.addListener((id) => {
-    browser.bookmarks.move(id, {
-        parentId: TOOLBAR_FOLDER_ID
-    });
+browser.bookmarks.onCreated.addListener((id, bookmark) => {
+    if (bookmark.parentId === UNFILED_ID) {
+        browser.bookmarks.move(id, {
+            parentId: TOOLBAR_FOLDER_ID
+        });
+    }
 });
